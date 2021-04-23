5221169079cc031a6cda297fb2258aef1e9cba5b
Megha S.r <m1064528@mindtree.com>
2021-04-23 14:21:49 +0530

    first commit

---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+        public name:string,
+        public phone:number,
+        public email:string,
+        public address:string,
+        public streetAddress:string,
+        public streetAddressline2:string,
+        
+
+    ){} 
+}
+
